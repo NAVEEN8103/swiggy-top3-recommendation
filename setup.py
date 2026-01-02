809d4e4c9fc760a9dda8352b1b13f525996a0c86
NAVEEN8103 <naveentiwari9425@gmail.com>
2026-01-02 21:02:21 +0530

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will recommend the top 3 food items from the swiggy home page',
+    author='Naveen Kumar Tiwari',
+    license='',
+)
